00e10732d02fc73734b50dd5f37b5dff2022c016
Parth Patel <parth2002715@gmail.com>
2021-09-24 12:30:10 -0400

    Add project files.

---
+++ IFOddOrEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IFOddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a digit: ");
+
+            var Num = Console.ReadLine();
+            var value = Convert.ToInt32(Num);
+            if (value % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else {
+                Console.WriteLine("The number is odd");
+            
+            }
+        }
+    }
+}
